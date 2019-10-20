88041c156a4578e80cf84f7e1eb935176e1ee61d
Chamin <chaminmax@gmail.com>
2019-10-20 17:09:44 +0530

    Update_/

--- part2/Transmit.java
+++ part2/Transmit.java
@@ -1,45 +1,55 @@
-//package iteration2;
-
 import java.net.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Transmit extends Main {
 
-    private final int packetsize = 1000;
-    private final int port = 55001;
-    private boolean stopPlay = false;
-    private MulticastSocket socket = null;
+    private final int packetsize = 256;
+    private final int port = 4445;
     private InetAddress host = null;
+    private MulticastSocket socket = null;
+    private byte tempBuffer[] = new byte[this.packetsize];
+    private boolean stopCapture = true;
 
-    @Override
-    public void run() {
-
+    private void captureAndSend() {
+        this.stopCapture = true;
         try {
-            // Construct the socket
-            this.socket = new MulticastSocket(this.port);
-            this.socket.joinGroup(this.host);
-            System.out.println("The server is ready");
-
-            // Create a packet
-            DatagramPacket packet = new DatagramPacket(new byte[this.packetsize], (this.packetsize));
-            this.playAudio();
-
+            int readCount;
             while (true) {
-                if (!this.stopPlay) {
-                    try {
+                if (!this.stopCapture) {
+                    readCount = getTargetDataLine().read(this.tempBuffer, 0, this.tempBuffer.length);  //capture sound into tempBuffer
 
-                        // Receive a packet (blocking)
-                        this.socket.receive(packet);
+                    if (readCount > 0) {
 
-                        // Print the packet
-                        this.getSourceDataLine().write(packet.getData(), 0, this.packetsize); //playing the audio  
-                        
-                        packet.setLength(this.packetsize);
+                        // Construct the datagram packet
+                        DatagramPacket packet = new DatagramPacket(this.tempBuffer, this.tempBuffer.length, this.host, 4446);
 
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                        // Send the packet
+                        this.socket.send(packet);
                     }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void stopCapture(){
+        this.stopCapture = true;
+    }
+    
+    public void startCapture(){
+        this.stopCapture = false;
+    }
+
+    public void run() {
+        try {
+            this.socket = new MulticastSocket();
+            this.socket.joinGroup(this.host);
+            this.captureAudio();
+            this.captureAndSend();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -47,16 +57,54 @@ public class Transmit extends Main {
             this.socket.close();
         }
     }
-    
+
     public Transmit(InetAddress host) {
         this.host = host;
     }
-    
-    public void stopPlay() {
-        this.stopPlay = true;
+
+    public Transmit() {
+        super();
     }
-    
-    public void startPlay() {
-        this.stopPlay = false;
+
+    public static void main(String[] args) {
+
+        // Check the whether the arguments are given
+        if (args.length != 1) {
+            System.out.println("Peer IP missing!");
+            return;
+        }
+        
+        Transmit cap = null;
+        Recieve ply = null;
+
+        try {
+
+            cap = new Transmit(InetAddress.getByName(args[0]));
+            cap.start();
+
+            ply = new Recieve(InetAddress.getByName(args[0]));
+            ply.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Scanner in = new Scanner(System.in);
+        boolean state = true; // playing
+        
+        while(true && (cap != null) && (ply != null)){
+            in.nextLine();
+            if(state) {
+                cap.stopCapture();
+                ply.startPlay();
+                System.out.println("Playing...");
+                state = false;
+            } else {
+                ply.stopPlay();
+                cap.startCapture();
+                System.out.println("Capturing...");
+                state = true;
+            }
+        } 
     }
 }
